6311f5c4c5e60561ec2757fd14534dcc37e89ef0
Jawad <jawad@CyberPunk>
2016-05-20 17:42:25 +0600

    updated products model

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product
+
+class ProductModelAdmin(admin.ModelAdmin):
+	list_display = ["title", "developer", "publisher", "category", "price"]
+	search_fields = ["title", "developer", "publisher", "category"]
+	class Meta:
+		model = Product
+
+admin.site.register(Product, ProductModelAdmin)
--- src/products/models.py
+++ src/products/models.py
@@ -1,5 +1,49 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save
+from django.utils import timezone
+
+from django.utils.text import slugify
+
 from django.db import models
 
 # Create your models here.
+
+def upload_location(instance, filename):
+    filebase, extension = filename.split(".")
+    return "%s/%s.%s" %(instance.slug, instance.slug, extension)
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    image = models.FileField(upload_to=upload_location,null=True,blank=True)
+    slug = models.SlugField(unique=True)
+    developer = models.CharField(max_length=120,null=True)
+    publisher = models.CharField(max_length=120,null=True)
+    price = models.DecimalField(max_digits=5,decimal_places=2)
+    category = models.CharField(max_length=120)
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receiver, sender=Product)
