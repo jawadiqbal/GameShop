82ec1e197be7e5c0bd49b06e91a9ffd7dff504ad
Jawad <jawad@CyberPunk>
2016-05-21 04:18:08 +0600

    added searchable category

--- src/products/admin.py
+++ src/products/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product
+from .models import Product, Choice
 
 class ProductModelAdmin(admin.ModelAdmin):
-	list_display = ["title", "developer", "publisher", "category", "quantity", "price"]
-	search_fields = ["title", "developer", "publisher", "category"]
+	list_display = ["title", "developer", "publisher", "quantity", "price"]
+	search_fields = ["title", "developer", "publisher"]
 	class Meta:
 		model = Product
 
 admin.site.register(Product, ProductModelAdmin)
+admin.site.register(Choice) # Product.objects.filter(category__title="ACT") to find action games
--- src/products/models.py
+++ src/products/models.py
@@ -16,6 +16,26 @@ def upload_location(instance, filename):
     filebase, extension = filename.split(".")
     return "%s/%s.%s" %(instance.slug, instance.slug, extension)
 
+class Choice(models.Model):
+    CATEGORY_OPTIONS = (
+        ('ACT', 'Action'),
+        ('ADV', 'Adventure'),
+        ('IND', 'Indie'),
+        ('MP', 'Multiplayer'),
+        ('RPG', 'RPG'),
+        ('RAC', 'Racing'),
+        ('SIM', 'Simulation'),
+        ('SPO', 'Sports'),
+        ('STR', 'Strategy'),
+    )
+    title = models.CharField(max_length=3,choices=CATEGORY_OPTIONS)
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
 class Product(models.Model):
     title = models.CharField(max_length=120)
     image = models.FileField(upload_to=upload_location,null=True,blank=True)
@@ -23,7 +43,7 @@ class Product(models.Model):
     developer = models.CharField(max_length=120,null=True)
     publisher = models.CharField(max_length=120,null=True)
     price = models.DecimalField(max_digits=5,decimal_places=2,null=True)
-    category = models.CharField(max_length=120)
+    category = models.ManyToManyField(Choice)
     quantity = models.IntegerField(default=10)
 
     def __unicode__(self):
