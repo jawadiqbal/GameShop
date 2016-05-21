b8213d3b22af781f61557facd1a1121782bba1f0
mazid1 <mazidmailbox@gmail.com>
2016-05-22 05:05:45 +0600

    pagination added

--- src/products/views.py
+++ src/products/views.py
@@ -1,13 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 from .models import Product
 
 def product_home(request):
-    queryset = Product.objects.all()
+    queryset_list = Product.objects.all()
+    paginator = Paginator(queryset_list, 8) # Show 25 contacts per page
+    page_request_var = "page"
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
     context = {
         "object_list": queryset,
-        "title": "Game shop"
+        "title": "Game shop",
+        "page_request_var": page_request_var
     }
     return render(request, "index.html", context)
